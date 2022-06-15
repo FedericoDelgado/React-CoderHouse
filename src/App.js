9292c82d7c1ccc81764fee227f5c5c194e346b57
Federico Delgado <delgadofefe@gmail.com>
2022-06-15 20:18:44 -0300

    Primera entrega

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido!<code> Esta es  </code> la primera entrega.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.coderhouse.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Federico Delgado
         </a>
       </header>
     </div>
---
+++ src/Caja.js
@@ -0,0 +1,11 @@
+function Caja(props) {
+    return (
+      <div className="Caja">
+        <p>
+            Buenas noches! proximo Footer!{props.nombre}
+        </p>
+      </div>
+    );
+  }
+
+  export default Caja;
